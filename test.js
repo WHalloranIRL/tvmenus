4ec38619fae7f8c676dea641565c6be7f8659fc0
Wayne Halloran <whalloranirl@protonmail.com>
2024-07-12 15:09:57 +0100

    changed

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const imageUrls = [
-  "https://cors-anywhere.herokuapp.com/https://drive.google.com/uc?id=170j_v8uBKzKvUc6Hj1NWAiXYf5QWDDNO",
-  "https://cors-anywhere.herokuapp.com/https://drive.google.com/uc?id=17-Xv9EnL0zBXbj_GBP9oR0igrUVYIGRG",
-  "https://cors-anywhere.herokuapp.com/https://drive.google.com/uc?id=16aH0_904OXZzxAzuHGcqw8KW-4weODMG",
-  "https://cors-anywhere.herokuapp.com/https://drive.google.com/uc?id=16doNlXwyYF8WNykdjBQzf9s4bwnHOskT",
+  "https://drive.google.com/uc?id=170j_v8uBKzKvUc6Hj1NWAiXYf5QWDDNO",
+  "https://drive.google.com/uc?id=17-Xv9EnL0zBXbj_GBP9oR0igrUVYIGRG",
+  "https://drive.google.com/uc?id=16aH0_904OXZzxAzuHGcqw8KW-4weODMG",
+  "https://drive.google.com/uc?id=16doNlXwyYF8WNykdjBQzf9s4bwnHOskT",
 ];
 
 let currentIndex = 0;
