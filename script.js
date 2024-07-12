6cf109ac7c449ca2308a314824f7e3d270463e00
Wayne Halloran <whalloranirl@protonmail.com>
2024-07-12 15:14:42 +0100

    Updated

--- script.js
+++ script.js
@@ -2,34 +2,18 @@ let images = [];
 let currentIndex = 0;
 
 async function fetchImages() {
-  const folderId = "16_DHbZ-2xsXTruTn_dJqyegEfEsRyhXB"; // Replace with your Google Drive folder ID
-  const apiKey = "AIzaSyCwJLqEWAXLAkyYOvhEqKZ8kURae11Lq5k"; // Replace with your Google API key
+  const repoOwner = "whalloranirl"; // Replace with your GitHub username
+  const repoName = "tvmenus"; // Replace with your GitHub repository name
+  const imagesPath = "images"; // Replace with the path to your images folder in the repository
 
-  const encodedFolderId = encodeURIComponent(folderId);
-  const apiUrl = `https://www.googleapis.com/drive/v3/files?q='${encodedFolderId}'+in+parents&key=${apiKey}&fields=files(id,name,mimeType)`;
-
-  console.log("API URL:", apiUrl); // Log the API URL
-
+  const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${imagesPath}`;
   try {
     const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
     const data = await response.json();
-
-    console.log("API Response:", data);
-
-    images = data.files
-      .filter((file) => file.mimeType.startsWith("image/"))
-      .map((file) => `https://drive.google.com/uc?export=view&id=${file.id}`);
-
-    console.log("Filtered Image URLs:", images);
-
-    if (images.length > 0) {
-      changeImage();
-    } else {
-      console.error("No images found in the specified folder.");
-    }
+    images = data
+      .filter((file) => /\.(jpg|jpeg|png|gif)$/.test(file.name))
+      .map((file) => file.download_url);
+    changeImage();
   } catch (error) {
     console.error("Error fetching images:", error);
   }
@@ -38,7 +22,6 @@ async function fetchImages() {
 function changeImage() {
   if (images.length === 0) return;
   const imgElement = document.getElementById("image");
-  console.log("Changing image to:", images[currentIndex]);
   imgElement.src = images[currentIndex];
   currentIndex = (currentIndex + 1) % images.length;
 }
